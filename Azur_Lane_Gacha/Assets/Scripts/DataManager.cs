2da2945907578b947dda52d4a74c1cad9e6038c4
F2ronia <alfk0660@naver.com>
2018-04-11 17:11:14 +0900

    startup

---
+++ Azur_Lane_Gacha/Assets/Scripts/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class DataManager : MonoBehaviour {
+
+    protected TextAsset jsonData;
+    //
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Sdfsdf"))
+        {
+            LitJson.JsonData getData = LitJson.JsonMapper.ToObject(jsonData.text);
+        }
+    }
+}
