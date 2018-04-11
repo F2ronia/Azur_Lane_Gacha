7cc1c873ad966b598384e3c4b755b9eae3d2f47d
F2ronia <alfk0660@naver.com>
2018-04-11 20:02:59 +0900

    JsonEdit
    Load Json Text File
    (To fix the scripts for key's value)

--- Azur_Lane_Gacha/Assets/Scripts/DataManager.cs
+++ Azur_Lane_Gacha/Assets/Scripts/DataManager.cs
@@ -5,24 +5,40 @@ using LitJson;
 
 public class DataManager : MonoBehaviour {
 
+    [SerializeField]
     protected TextAsset jsonData;
-    //
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //읽어올 Json 텍스트 파일
 
     private void OnGUI()
     {
+        
         if(GUILayout.Button("Sdfsdf"))
         {
-            LitJson.JsonData getData = LitJson.JsonMapper.ToObject(jsonData.text);
+            JsonData getData =  JsonMapper.ToObject(jsonData.text);
+            //----------------------텍스트 파일을 JsonData 파일로 변환-----------------------
+            //------------------------------텍스트 파일의 구조-------------------------------
+            //--------------------[건조분류]-[함선종류]-<이름>,<시간>,<등급>-----------------
+            //------호출 시 [건조분류][건조분류번호][함선종류][함선종류번호][세부사항]-------
+
+            ICollection<string> i;
+
+            if (getData["Light"][0].ToString().Equals("Destroyer"))
+            {
+                Debug.Log("sdfsdfsdf");
+            }
+            else
+            {
+                i = getData["Light"][0].Keys;
+                  //키 값만 불러오고 싶다 시부럴..
+
+
+            }
+
+
+            Debug.Log(getData["Light"][0]["Destroyer"][0]["Name"].ToString());
+            Debug.Log(getData["Light"][0]["Destroyer"][0]["Time"].ToString());
+            Debug.Log(getData["Light"][1]["Light_Cruiser"][0]["Name"].ToString());
+            Debug.Log(getData["Light"][1]["Light_Cruiser"][0]["Time"].ToString());
         }
     }
 }
